b18664500ea5baeacf27546c8f4e208645cc8ad3
SparksFullStack <csparks@antioch.edu>
2018-08-17 14:32:40 -0700

    added notes for clarification in CommentBox.test.js

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -1,14 +1,19 @@
+// importing React and the Full DOM Render method from Enzyme
 import React from 'react';
 import { mount } from 'enzyme';
 
+// importing the component to test
 import CommentBox from 'components/CommentBox';
 
+// declaring the component mounting variable in the global scope
 let wrapped;
 
+// mounting the component to test before each individual test
 beforeEach(() => {
     wrapped = mount(<CommentBox />);
 })
 
+// testing for the existence of the 'textarea' and 'button' elements inside of the component
 it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
