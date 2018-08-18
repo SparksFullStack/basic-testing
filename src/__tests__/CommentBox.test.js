a4c91a8fe1a3fc7ed7260c884f03f37c2392b650
SparksFullStack <csparks@antioch.edu>
2018-08-17 17:51:40 -0700

    consolidated two seperate tests into a single one in CommentBox.test.js

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -21,32 +21,51 @@ it('has a text area and a button', () => {
 })
 
 // testing that users can enter text into the 'textarea' element
-it('has a textarea element that users can type inside of', () => {
-    // finding the 'textarea' element so we have a reference to it
+    // it('has a textarea element that users can type inside of', () => {
+    //     // finding the 'textarea' element so we have a reference to it
+    //     wrapped.find('textarea')
+    //         // simulating a change and passing in a static value for the event object
+    //         .simulate('change', { target: {
+    //             value: "cheesedick"
+    //         }})
+        
+    //     wrapped.update();
+
+    //     // testing that the value passed in through the mock event did get set
+    //     expect(wrapped.find('textarea').prop('value')).toEqual('cheesedick');
+    // })
+
+    // // testing that the form can successfully submit and is set equal to an empty string afterwards
+    // it('should be set equal to an empty string after submittal', () =>{ 
+    //     // setting the value of the text area so that it can be submitted
+    //     wrapped.find('textarea')
+    //         .simulate('change', { target: { value: "cheesedick" } });
+    //     wrapped.update();
+
+    //     // simulating the submission of the form
+    //     wrapped.find('form')
+    //         .simulate('submit');
+    //     wrapped.update();
+
+    //     // expecting the text area to have an empty string as its
+    //     expect(wrapped.find('textarea').prop('value')).toEqual('');
+    // })
+
+// * NOTE: this function could take the place of both of the above tests as it consolidates the logic of both:
+it('should be able to modify and submit values and should have a value of an empty string after submittal', () => {
+    // simulating text user input
     wrapped.find('textarea')
-        // simulating a change and passing in a static value for the event object
-        .simulate('change', { target: {
-            value: "cheesedick"
-        }})
-    
+        .simulate('change', { target: { value: 'cheesedick' } });
     wrapped.update();
 
-    // testing that the value passed in through the mock event did get set
+    // asserting that the value should have changed
     expect(wrapped.find('textarea').prop('value')).toEqual('cheesedick');
-})
 
-// testing that the form can successfully submit and is set equal to an empty string afterwards
-it('should be set equal to an empty string after submittal', () =>{ 
-    // setting the value of the text area so that it can be submitted
-    wrapped.find('textarea')
-        .simulate('change', { target: { value: "cheesedick" } });
-    wrapped.update();
-
-    // simulating the submission of the form
+    // simulating the submittal of the form
     wrapped.find('form')
         .simulate('submit');
     wrapped.update();
 
-    // expecting the text area to have an empty string as its
+    // asserting that the value should have zeroed out
     expect(wrapped.find('textarea').prop('value')).toEqual('');
 })
