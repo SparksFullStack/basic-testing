8d269c7cd43a61a359704fef854eda7c00e9bf5a
SparksFullStack <csparks@antioch.edu>
2018-08-21 16:12:26 -0700

    added an unknown action type test for commentsReducer

--- src/reducers/__tests__/commentsReducer.test.js
+++ src/reducers/__tests__/commentsReducer.test.js
@@ -16,3 +16,12 @@ it('should handle actions with a type of SAVE_COMMENT', () => {
     // making an assertion about the state that should be returned from the reducer
     expect(newState).toEqual(["New Comment"]);
 })
+
+// opening an 'it' block to test an unknown type passed to the reducer
+it('should handle an action with an unknown type', () => {
+    // calling the reducer with an empty array as the initial state and no values in the action
+    const newState = commentsReducer([], {});
+
+    // asserting that the call to the reducer should return an empty array
+    expect(newState).toEqual([]);
+})
